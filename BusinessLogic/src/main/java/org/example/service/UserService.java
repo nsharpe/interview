68e6e4b1ad0fa06df7ebc32a260ca5464ea99676
neil <neil@nope.com>
2025-12-28 13:40:40 -0800

    enhance: Add lombok and test integration module

--- BusinessLogic/src/main/java/org/example/service/UserService.java
+++ BusinessLogic/src/main/java/org/example/service/UserService.java
@@ -1,15 +1,13 @@
 package org.example.service;
 
+import lombok.RequiredArgsConstructor;
 import org.example.BadInput;
 import org.example.repository.UserRepository;
 
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     public UserModel getUser(long id){
         if(id <= 0){
---
+++ IntegrationTests/src/main/java/org/example/test/util/TestContainers.java
@@ -0,0 +1,7 @@
+package org.example.test.util;
+
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class TestContainers {
+}
